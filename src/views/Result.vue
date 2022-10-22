59cd703b561613ace761727a52e4e4a0a8877db0
Andy Chang <andy.rochi@gmail.com>
2022-10-23 07:50:12 +0800

    feat: Complete result page data flow

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -10,7 +10,7 @@
           <div class="">
             <div>Golden Image</div>
             <div class="image-reference">
-              <img :src="images[0]" class="w-50" />
+              <img :src="images[8]" class="w-50" />
             </div>
           </div>
           <div class="">
@@ -80,23 +80,19 @@
               <input
                 class="form-check-input"
                 type="checkbox"
-                v-model="boundingBox1Selected"
-                id="boundingBox1"
+                v-model="hollowSelected"
+                id="hollow"
               />
-              <label class="form-check-label" for="boundingBox1">
-                BoundingBox1
-              </label>
+              <label class="form-check-label" for="hollow"> hollow </label>
             </div>
             <div class="form-check">
               <input
                 class="form-check-input"
                 type="checkbox"
-                v-model="boundingBox2Selected"
-                id="boundingBox2"
+                v-model="buldgeSelected"
+                id="buldge"
               />
-              <label class="form-check-label" for="boundingBox2">
-                BoundingBox2
-              </label>
+              <label class="form-check-label" for="buldge"> buldge </label>
             </div>
           </div>
         </div>
@@ -150,8 +146,9 @@ export default {
   data() {
     return {
       goldenImageSelected: true,
-      boundingBox1Selected: false,
-      boundingBox2Selected: false,
+      hollowSelected: false,
+      buldgeSelected: false,
+      images: [],
     };
   },
   computed: {
@@ -159,44 +156,44 @@ export default {
       console.log("called");
       if (
         this.goldenImageSelected &&
-        !this.boundingBox1Selected &&
-        !this.boundingBox2Selected
+        !this.hollowSelected &&
+        !this.buldgeSelected
       ) {
         return 1;
       } else if (
         !this.goldenImageSelected &&
-        this.boundingBox1Selected &&
-        !this.boundingBox2Selected
+        this.hollowSelected &&
+        !this.buldgeSelected
       ) {
         return 2;
       } else if (
         this.goldenImageSelected &&
-        this.boundingBox1Selected &&
-        !this.boundingBox2Selected
+        this.hollowSelected &&
+        !this.buldgeSelected
       )
         return 3;
       else if (
         !this.goldenImageSelected &&
-        !this.boundingBox1Selected &&
-        this.boundingBox2Selected
+        !this.hollowSelected &&
+        this.buldgeSelected
       )
         return 4;
       else if (
         this.goldenImageSelected &&
-        !this.boundingBox1Selected &&
-        this.boundingBox2Selected
+        !this.hollowSelected &&
+        this.buldgeSelected
       )
         return 5;
       else if (
         !this.goldenImageSelected &&
-        this.boundingBox1Selected &&
-        this.boundingBox2Selected
+        this.hollowSelected &&
+        this.buldgeSelected
       )
         return 6;
       else if (
         this.goldenImageSelected &&
-        this.boundingBox1Selected &&
-        this.boundingBox2Selected
+        this.hollowSelected &&
+        this.buldgeSelected
       )
         return 7;
       else return 0;
@@ -205,25 +202,38 @@ export default {
   setup() {
     // Image source
     let imgsrc;
-    const images = [];
     try {
       //Check availability
       imgsrc = ref(require("@/assets/images/about.jpg"));
     } catch (error) {
       console.error(error);
     }
-    const templateString =
-      "https://dummyimage.com/1024x1024/000000/ffffff.png&text=Test";
-    for (let i = 0; i < 12; i++) {
-      images.push(templateString + i);
-    }
     return {
       imgsrc,
-      images,
     };
   },
   mounted() {
     console.log("images debug:", this.images);
+
+    const templateString =
+      "https://dummyimage.com/1024x1024/000000/ffffff.png&text=Test";
+    if (this.golden_image === "") {
+      for (let i = 0; i < 12; i++) {
+        this.images.push(templateString + i);
+      }
+    } else {
+      this.images = [
+        this.defect_image, // 0
+        this.sem_gds, // 1
+        this.sem_r_rect, // 2
+        this.sem_gds_r_rect, // 3
+        this.sem_g_rect, // 4
+        this.sem_gds_g_rect, // 5
+        this.sem_rg_rect, // 6
+        this.sem_gds_rg_rect, // 7
+        this.golden_image, // 8
+      ];
+    }
   },
 };
 </script>
