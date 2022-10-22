b2b1842fb5d0d08f732a8e4382aee657e36cd628
Andy Chang <andy.rochi@gmail.com>
2022-10-23 06:53:10 +0800

    feat: Complet result page layout

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,36 +1,104 @@
 <template>
   <div class="content py-sm-5 bg-custom">
-    <div class="container-fluid pt-5 mt-5">
+    <div class="container-fluid mt-5 width-limit">
+      <!-- Top Row -->
       <div class="row p-5 text-light">
-        <!-- About Title -->
-        <h1 class="title text-start fw-bold">About Us</h1>
-        <!-- Column -->
-        <div class="col-sm px-5">
-          <h4 class="text-capitalize text-start mt-5 mb-3 style fw-bold">
-            What is MK template
-          </h4>
-          <p class="text-other">
-            Questo testo ha uno stile con alcune delle proprietà di
-            formattazione del testo. L'intestazione utilizza le proprietà di
-            allineamento del testo, trasformazione del testo e colore. Il
-            paragrafo è rientrato, allineato e viene specificato lo spazio tra i
-            caratteri. La sottolineatura viene rimossa da questo colore.Questo
-            testo ha uno stile con alcune delle proprietà di formattazione del
-            testo. L'intestazione utilizza le proprietà di allineamento del
-            testo, trasformazione del testo e colore. Il paragrafo è rientrato,
-            allineato e viene specificato lo spazio tra i caratteri. La
-            sottolineatura viene rimossa da questo colore.
-          </p>
-          <a
-            href="https://github.com/mohammedmoutawakkil/MKTemplate"
-            class="btn text-uppercase text-light fw-bold px-sm-4 py-sm-3 Stars"
-            >Github Star</a
-          >
+        <h1 class="title fw-bold">Defect Inspection Results</h1>
+      </div>
+      <div class="row text-light mb-5">
+        <div class="col-sm-4 d-flex flex-column justify-content-center">
+          <div class="">
+            <div>Golden Image</div>
+            <div class="image-reference">
+              <img :src="images[0]" class="w-50" />
+            </div>
+          </div>
+          <div class="">
+            <div>Image with Defect</div>
+            <div class="image-reference">
+              <img :src="images[0]" class="w-50" />
+            </div>
+          </div>
         </div>
-        <!-- Column -->
-        <div class="col-sm pb-3 slanted mt-5 mt-md-5 mt-lg-0">
-          <img :src="imgsrc" class="w-100 rounded" alt="about image " />
-          <img :src="data['sem_gds']" />
+        <div class="col-sm-4 d-flex justify-content-end main-col">
+          <img v-if="imageStatus == 1" class="main-image" :src="images[1]" />
+          <img
+            v-else-if="imageStatus == 2"
+            class="main-image"
+            :src="images[2]"
+          />
+          <img
+            v-else-if="imageStatus == 3"
+            class="main-image"
+            :src="images[3]"
+          />
+          <img
+            v-else-if="imageStatus == 4"
+            class="main-image"
+            :src="images[4]"
+          />
+          <img
+            v-else-if="imageStatus == 5"
+            class="main-image"
+            :src="images[5]"
+          />
+          <img
+            v-else-if="imageStatus == 6"
+            class="main-image"
+            :src="images[6]"
+          />
+          <img
+            v-else-if="imageStatus == 7"
+            class="main-image"
+            :src="images[7]"
+          />
+          <img v-else class="main-image" :src="images[0]" />
+        </div>
+        <div
+          class="
+            col-sm-4
+            d-flex
+            flex-column
+            justify-content-center
+            align-items-center
+          "
+        >
+          <div class="text-start">選擇要檢視的內容標記</div>
+          <div class="form-container w-50 ml-5">
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                v-model="goldenImageSelected"
+                id="goldenImage"
+              />
+              <label class="form-check-label" for="goldenImage">
+                Golden Image
+              </label>
+            </div>
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                v-model="boundingBox1Selected"
+                id="boundingBox1"
+              />
+              <label class="form-check-label" for="boundingBox1">
+                BoundingBox1
+              </label>
+            </div>
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                v-model="boundingBox2Selected"
+                id="boundingBox2"
+              />
+              <label class="form-check-label" for="boundingBox2">
+                BoundingBox2
+              </label>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -47,22 +115,83 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return {
+      goldenImageSelected: true,
+      boundingBox1Selected: false,
+      boundingBox2Selected: false,
+    };
+  },
+  computed: {
+    imageStatus() {
+      console.log("called");
+      if (
+        this.goldenImageSelected &&
+        !this.boundingBox1Selected &&
+        !this.boundingBox2Selected
+      ) {
+        return 1;
+      } else if (
+        !this.goldenImageSelected &&
+        this.boundingBox1Selected &&
+        !this.boundingBox2Selected
+      ) {
+        return 2;
+      } else if (
+        this.goldenImageSelected &&
+        this.boundingBox1Selected &&
+        !this.boundingBox2Selected
+      )
+        return 3;
+      else if (
+        !this.goldenImageSelected &&
+        !this.boundingBox1Selected &&
+        this.boundingBox2Selected
+      )
+        return 4;
+      else if (
+        this.goldenImageSelected &&
+        !this.boundingBox1Selected &&
+        this.boundingBox2Selected
+      )
+        return 5;
+      else if (
+        !this.goldenImageSelected &&
+        this.boundingBox1Selected &&
+        this.boundingBox2Selected
+      )
+        return 6;
+      else if (
+        this.goldenImageSelected &&
+        this.boundingBox1Selected &&
+        this.boundingBox2Selected
+      )
+        return 7;
+      else return 0;
+    },
+  },
   setup() {
     // Image source
     let imgsrc;
-
+    const images = [];
     try {
       //Check availability
       imgsrc = ref(require("@/assets/images/about.jpg"));
     } catch (error) {
       console.error(error);
     }
+    const templateString =
+      "https://dummyimage.com/1024x1024/000000/ffffff.png&text=Test";
+    for (let i = 0; i < 12; i++) {
+      images.push(templateString + i);
+    }
     return {
       imgsrc,
+      images,
     };
   },
   mounted() {
-    console.log(this.$route);
+    console.log("images debug:", this.images);
   },
 };
 </script>
@@ -76,8 +205,6 @@ p:first-child {
 }
 
 p {
-  text-align: justify;
-  letter-spacing: 3px;
   line-height: 1.6;
   text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
 }
@@ -91,5 +218,14 @@ p {
 }
 .slanted > img {
   box-shadow: 10px 7px 10px black;
+}
+
+.main-col {
+  flex-basis: 1 !important;
+}
+
+.main-image {
+  width: 100%;
+  object-fit: contain;
 }
 </style>
